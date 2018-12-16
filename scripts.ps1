f7a225c303d045803f5cc41a7ed29ef713fac0bf
lucian <lucian.stoica21@gmail.com>
2018-12-16 20:36:39 +0100

    first powershell scripts

---
+++ scripts.ps1
@@ -0,0 +1,23 @@
+﻿Connect-AzureRmAccount
+Get-AzureRmSubscription
+Get-AzureRmResourceGroup
+
+New-AzureRmResourceGroup -Name prod -Location 'eastus'
+
+
+New-AzureRmResourceGroupDeployment -Name testVNETDeploy -ResourceGroupName prod -TemplateFile C:\projects\Azure\azure_templates\VNET\101-vnet-two-subnets\azuredeploy.json -TemplateParameterFile C:\projects\Azure\azure_templates\VNET\101-vnet-two-subnets\azuredeploy.parameters.json
+
+
+Get-AzureRmResource -ResourceGroupName prod
+
+
+############################################################################
+Get-AzureRmResourceGroup | select resourcegroupname
+$rg = Get-AzureRmResourceGroup | where resourcegroupname -like Group* | Remove-AzureRmResourceGroup
+Get-AzureRmResource | Select-Object name,resourcegroupname
+remove-azure
+
+$PSVersionTable
+
+Update-Module -Force
+
